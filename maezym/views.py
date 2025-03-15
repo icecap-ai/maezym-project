7ade707dfc2a69b1ed24f01b55df131aa5bf86dd
jinmo-KANG <mommoo@naver.com>
2025-03-15 21:17:14 +0900

    Add Reservation model and Reservation API endpoints

--- maezym/views.py
+++ maezym/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Reservation
+from .serializers import ReservationSerializer
 
-# Create your views here.
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from maezym.views import ReservationViewSet         # 여기서 "maezym"은 생성한 앱 이름
+
+router = routers.DefaultRouter()
+router.register(r'reservations', ReservationViewSet)
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+
